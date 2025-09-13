08c7ce5e3d9663b026c4b4bce4a4bd1ec5b9b206
pnnh <linyangz@outlook.com>
2025-09-13 14:10:33 +0800

    初始化新的RN For Windows项目，跑通示例并添加RSD示例按钮运行成功

---
+++ windows/caedesk/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::caedesk::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::caedesk::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
